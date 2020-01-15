d62c98541f4160a2217ff0923a80b2939ff7935d
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-01-15 10:05:38 +0200

    Add redux action

---
+++ src/components/CakeContainer/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const cakeContainer = (props) => {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy Cake</button>
+        </div>
+    );
+};
+
+export default cakeContainer;
