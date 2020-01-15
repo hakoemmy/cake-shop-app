a7e74812813a357b738f70303703bce6b150e77d
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-01-15 10:34:07 +0200

    Add Redux Strore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CakeContainer from './components/CakeContainer/CakeContainer';
+
 
 function App() {
   return (
