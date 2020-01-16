c3e73ce1acc8c9cf983871e12d7bf790e947364a
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-01-16 09:50:50 +0200

    Add mapDispatchToProps with 2nd argument(ownProps)

--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../../redux';
 
 const ItemContainer = (props) => {
     return (
         <div>
             <h2>Item - { props.item }</h2>
+            <button onClick={props.buyItem}> Buy</button>
         </div>
     );
 }
@@ -19,4 +21,13 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps) (ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+   const dispatchFunction = ownProps.cake
+                        ? () => dispatch(buyCake())
+                        : () => dispatch(buyIceCream())
+     return {
+         buyItem: dispatchFunction
+     }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
